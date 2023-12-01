b8ca6421957ea2dd0b88b48bfca1c9497e5a19fe
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2023-12-01 13:11:39 -0600

    feat: Day -1 - Task 2 (bonus)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn fake_error() -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(fake_error));
 
     Ok(router.into())
 }
