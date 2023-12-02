601162e70222e77b2dcca20847cdf3af2047e4df
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2023-12-01 20:11:35 -0600

    feat: 1.1 Cube the bits

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,11 +8,18 @@ async fn fake_error() -> impl IntoResponse {
     (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
 }
 
+// Day 1 - Task 1
+async fn recalibrate(Path((num1, num2)): Path<(i32, i32)>) -> impl IntoResponse {
+    let result = (num1 ^ num2).pow(3);
+    (StatusCode::OK, result.to_string())
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(fake_error));
+        .route("/-1/error", get(fake_error))
+        .route("/1/:num1/:num2", get(recalibrate));
 
     Ok(router.into())
 }
